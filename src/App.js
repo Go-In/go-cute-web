d54be523e16e0971165f57676b9e352ede8c41e8
komcal <kom.sujautra@gmail.com>
2018-05-01 14:28:15 +0700

    init file

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+  Sigma,
+  LoadGEXF,
+  RandomizeNodePositions,
+  RelativeSize,
+} from 'react-sigma';
+import ForceLink from 'react-sigma/lib/ForceLink'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  onOverNode = (e) => {
+    console.log("Mouse over node: " + e.data.node.label);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="graph-wrapper">
+          <Sigma
+            onOverNode={this.onOverNode}
+            style={{
+              height: '900px',
+            }}
+          >
+            <LoadGEXF path="/following_small.gefx">
+              <RandomizeNodePositions>
+                <ForceLink iterationsPerRender={1} background={true} easing="cubicInOut" timeout={5000}/>
+                <RelativeSize initialSize={15}/>
+              </RandomizeNodePositions>
+            </LoadGEXF>
+          </Sigma>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
